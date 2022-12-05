cab8fa1c90f6700be96d6a7d2df8ca5dbddd0474
Yura Karbushev <yurakarbushev@gmail com>
2022-12-05 13:58:15 +0700

    solution of problem №10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(number);
+
+Console.WriteLine("Вторая цифра = " + stringNumber[1]);
+
